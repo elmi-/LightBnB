9bde05c70acde1c7bb31dc8b839806d09631d576
elmi- <abdi.elmi@rocketmail.com>
2021-11-25 17:53:57 -0800

    fixed 01_seeds test user password for easier testing

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name,email,password)
 VALUES
-  ('Testing LighBnB User', 'test@test.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+  ('Testing LighBnB User', 'test@test.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES
